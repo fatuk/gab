3ed98b3a3a31b8efe7dc0ab79b48175835029b38
fatuk <fatukk@gmail.com>
2015-12-28 17:06:18 +0300

    Rename initiatives

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -16,21 +16,21 @@
 		$scope.q1.budget = 0;
 		$scope.q1.quiz = 0;
 		$scope.q1.eventMoney = 0;
-		$scope.q1.initiativePlus = 100000;
-		$scope.q1.initiativeMinus = 50000;
+		$scope.q1.initiativeRevenue = 100000;
+		$scope.q1.initiativeExpense = 50000;
 		$scope.q1.activity = 0;
 
 		$scope.q2.budget = 0;
 		$scope.q2.quiz = 0;
 		$scope.q2.eventMoney = 0;
-		$scope.q2.initiativePlus = 125000;
-		$scope.q2.initiativeMinus = 50000;
+		$scope.q2.initiativeRevenue = 125000;
+		$scope.q2.initiativeExpense = 50000;
 		$scope.q2.activity = 0;
 
 
 		$scope.$watch('q1', function () {
-			var q1Operations = $scope.q1.initiativePlus - $scope.q1.initiativeMinus + $scope.q1.startBalance,
-				q1Bonus = ($scope.q1.initiativePlus - $scope.q1.initiativeMinus + $scope.q1.startBalance) * 0.1;
+			var q1Operations = $scope.q1.initiativeRevenue - $scope.q1.initiativeExpense + $scope.q1.startBalance,
+				q1Bonus = ($scope.q1.initiativeRevenue - $scope.q1.initiativeExpense + $scope.q1.startBalance) * 0.1;
 			$scope.q1OperationsAndBonus = q1Operations + q1Bonus;
 		}, true);
 
